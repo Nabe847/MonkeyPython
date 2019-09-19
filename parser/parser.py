3db7b737b135d9ce109a9c9a24ec1ec08401dbca
manabe-t <manabetakumi02@gmail.com>
2019-09-20 06:37:06 +0900

    add parserの定義を追加

---
+++ parser/parser.py
@@ -0,0 +1,17 @@
+from ast import ast
+
+
+class Parser:
+    def __init__(self, l):
+        self.lexer = l
+        self.cur_token = None
+        self.peek_token = None
+        self.next_token()
+        self.next_token()
+
+    def next_token(self):
+        self.cur_token = self.peek_token
+        self.peek_token = self.lexer.next_token()
+
+    def parse_program(self):
+        pass
